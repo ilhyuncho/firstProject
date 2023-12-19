5c8fefc8caa8c6bc6102edb944b0ee93b94fae42
choilhyun <cihg155@gmail.com>
2023-12-19 17:21:00 +0900

    first commit

---
+++ src/main/java/com/example/firstporject/FirstPorjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.firstporject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstPorjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstPorjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/firstporject/FirstPorjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.firstporject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstPorjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
