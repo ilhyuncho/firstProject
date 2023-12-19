3078df6387cf7e7ad7181bf5af7f0f20ceaa78c4
choilhyun <cihg155@gmail.com>
2023-12-19 22:41:56 +0900

    [view] db get정보 view에 보이기, mustache 문법  {{#article}},  {{/article}}

--- src/main/java/com/example/firstporject/controller/ArticleController.java
+++ src/main/java/com/example/firstporject/controller/ArticleController.java
@@ -6,8 +6,12 @@ import com.example.firstporject.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Optional;
 
 @Slf4j
 @Controller
@@ -32,6 +36,15 @@ public class ArticleController {
         //System.out.println(saved.toString());
 
         return "";
+    }
+
+    @GetMapping("/articles/{id}")
+    String getArticle(@PathVariable("id") Long id, Model model){
+
+        Article article = articleRepository.findById(id).orElse(null);
+
+        model.addAttribute("article", article);
+        return "articles/show";
     }
 
 }
